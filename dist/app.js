622e234a14b664c97d0b18a2e5997bc040dc780b
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2026-02-11 20:19:26 -0300

    Aula Typescript

---
+++ dist/app.js
@@ -0,0 +1,52 @@
+// 2. Classe para experiência profissional
+class ExperienciaProfissional {
+    constructor(titulo, empresa, periodo) {
+        this.titulo = titulo;
+        this.empresa = empresa;
+        this.periodo = periodo;
+    }
+    renderizar() {
+        const div = document.createElement('div');
+        div.className = 'item-curriculo experiencia';
+        div.innerHTML = `
+            <h3> ${this.titulo}</h3>
+            <p><strong>Empresa:</strong> ${this.empresa}</p>
+            <p><strong>Período:</strong> ${this.periodo}</p>
+        `;
+        return div;
+    }
+}
+// 3. Classe para formação acadêmica
+class FormacaoAcademica {
+    constructor(titulo, instituicao, anoConclusao) {
+        this.titulo = titulo;
+        this.instituicao = instituicao;
+        this.anoConclusao = anoConclusao;
+    }
+    renderizar() {
+        const div = document.createElement('div');
+        div.className = 'item-curriculo formacao';
+        div.innerHTML = `
+            <h3> ${this.titulo}</h3>
+            <p><strong>Instituição:</strong> ${this.instituicao}</p>
+            <p><strong>Concluído em:</strong> ${this.anoConclusao}</p>
+        `;
+        return div;
+    }
+}
+// 4. Manipulação do Dom (adicionando dinâmica)
+const container = document.getElementById("conteudo-curriculo");
+// Criar um array que aceite qualquer coisa que assine o contrato do curriculo
+const meuCurriculo = [
+    new ExperienciaProfissional("Desenvolvedor Front-end", "Tech Solutions", "2023 - Presente"),
+    new FormacaoAcademica("Análise e Desenvolvimento de Sistemas", "Universidade de São Paulo", "2022"),
+    new ExperienciaProfissional("Estagiário de TI", "Ada Tech", "2021 - 2023")
+];
+// Iterando sobre os itens e jogando no HTML
+if (container) {
+    meuCurriculo.forEach(item => {
+        container.appendChild(item.renderizar());
+    });
+}
+export {};
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,65 @@
+// 1. A Interface (o contrato)
+interface ItemCurriculo {
+    titulo: string;
+    renderizar(): HTMLDivElement;
+}
+
+// 2. Classe para experiência profissional
+
+class ExperienciaProfissional implements ItemCurriculo {
+
+    constructor(
+        public titulo: string,
+        public empresa: string,
+        public periodo: string
+    ){}
+
+    renderizar(): HTMLDivElement {
+        const div = document.createElement('div');
+        div.className = 'item-curriculo experiencia';
+        div.innerHTML = `
+            <h3> ${this.titulo}</h3>
+            <p><strong>Empresa:</strong> ${this.empresa}</p>
+            <p><strong>Período:</strong> ${this.periodo}</p>
+        `;
+        return div;
+    }
+}
+
+// 3. Classe para formação acadêmica
+class FormacaoAcademica implements ItemCurriculo {
+
+    constructor(
+        public titulo: string,
+        public instituicao: string,
+        public anoConclusao: string
+    ){}
+
+    renderizar(): HTMLDivElement {
+        const div = document.createElement('div');
+        div.className = 'item-curriculo formacao';
+        div.innerHTML = `
+            <h3> ${this.titulo}</h3>
+            <p><strong>Instituição:</strong> ${this.instituicao}</p>
+            <p><strong>Concluído em:</strong> ${this.anoConclusao}</p>
+        `;
+        return div;
+    }
+}
+
+// 4. Manipulação do Dom (adicionando dinâmica)
+const container = document.getElementById("conteudo-curriculo");
+
+// Criar um array que aceite qualquer coisa que assine o contrato do curriculo
+const meuCurriculo: ItemCurriculo[] = [
+    new ExperienciaProfissional("Desenvolvedor Front-end", "Tech Solutions", "2023 - Presente"),
+    new FormacaoAcademica("Análise e Desenvolvimento de Sistemas", "Universidade de São Paulo", "2022"),
+    new ExperienciaProfissional("Estagiário de TI", "Ada Tech", "2021 - 2023")
+];
+
+// Iterando sobre os itens e jogando no HTML
+if (container) {
+    meuCurriculo.forEach(item => {
+        container.appendChild(item.renderizar());
+    });
+}
